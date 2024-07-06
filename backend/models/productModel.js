a3295240e513bc11ee768266a2380d005727eb54
arnoldmn <arnoldnzibuk@gmail.com>
2024-07-06 20:17:20 +0300

    Fix the user & product model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,18 +1,71 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose';
 
-const productSchema = mongoose.Schema({
-    productName : String,
-    brandName : String,
-    category : String,
-    productImage : [],
-    description : String,
-    price : Number,
-    sellingPrice : Number
-},{
-    timestamps : true
-})
+const { Schema } = mongoose;
 
+const ProductSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    richDescription: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    images: [{
+        type: String
+    }],
+    brand: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    stockQuantity: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    numReviews: {
+        type: Number,
+        default: 0,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
-const productModel = mongoose.model("product",productSchema)
+ProductSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
 
-module.exports = productModel
+ProductSchema.set('toJSON', {
+    virtuals: true,
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+
+export { Product, ProductSchema };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,71 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose';
 
+const { Schema } = mongoose;
 
-const userSchema = new mongoose.Schema({
-    name : String,
-    email : {
-        type : String,
-        unique : true,
-        required : true
+const ProductSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
     },
-    password : String,
-    profilePic : String,
-    role : String,
-},{
-    timestamps : true
-})
+    description: {
+        type: String,
+        required: true
+    },
+    richDescription: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    images: [{
+        type: String
+    }],
+    brand: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    stockQuantity: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    numReviews: {
+        type: Number,
+        default: 0,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
+ProductSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
 
-const userModel =  mongoose.model("user",userSchema)
+ProductSchema.set('toJSON', {
+    virtuals: true,
+});
 
+const Product = mongoose.model('Product', ProductSchema);
 
-module.exports = userModel
+export { Product, ProductSchema };
